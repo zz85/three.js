ac49f2e9d97cd69cdcd9e6dfa134742d0b4faf7c
zz85 <zz85nus@gmail.com>
2011-12-24 02:37:24 +0800

    refactoring for intro

--- examples/ChristmasSpecial2011/main.js
+++ examples/ChristmasSpecial2011/main.js
@@ -9,40 +9,7 @@ if ( !Detector.webgl ) {
 
 
 function render() {
-
-	var time = Date.now() * 0.00005;
-
-	// camera.position.x += ( mouseX - camera.position.x ) * 0.05;
-	// camera.position.y += ( - mouseY - camera.position.y ) * 0.05;
-	
-	// camera.position.z -= 0.05;
-	// camera.position.z -= 2;
-	// camera.position.z -= 15;
-	
-	var position; 
-	for ( i = 0; i < starFieldsCount; i ++ ) {
-		position = starFields.children[i].position;
-		position.z += 10;
-		
-		if (position.z > 400) { 
-			// use larger neg number to keep distance from star fields, but compensate with closer star fields
-			console.log('ping!');
-			position.z -= starFieldLength ;
-		}
-	}
-
-	
-	// Vary Brightness
-	for( i = 0; i < starFieldMaterials.length; i ++ ) {
-		
-		starFieldMaterials[i].color.setHSV( 0, 0, Math.sin( time * 100 + i* 50) * 0.5  * 0.2 + 0.6) ;
-		// starFieldMaterials[i].color.setHSV( 0, 0, 0.4 ) ;
-	
-	}
-	
-	renderer.clear();
-	renderer.render( scene, camera );
-
+	if (renderCallback) renderCallback();
 }
 
 function animate() {
--- examples/ChristmasSpecial2011/sceneIntro.js
+++ examples/ChristmasSpecial2011/sceneIntro.js
@@ -1,3 +1,139 @@
+var starFieldLength = 2000;
+var starFieldsCount = 3;
+var starFields = new THREE.Object3D();
+
+
+function renderIntro() {
+	// Render
+	var time = Date.now() * 0.00005;
+
+	camera.position.x += ( mouseX * 0.15 - camera.position.x ) * 0.04;
+	camera.position.y += ( - mouseY * 0.15	 - camera.position.y ) * 0.04;
+	
+
+	var position; 
+	for ( i = 0; i < starFieldsCount; i ++ ) {
+		position = starFields.children[i].position;
+		position.z += 1;
+		
+		if (position.z > 400) { 
+			position.z -= starFieldLength ;
+		}
+	}
+
+	
+	// Vary Brightness
+	for( i = 0; i < starFieldMaterials.length; i ++ ) {
+		
+		starFieldMaterials[i].color.setHSV( 0, 0, Math.sin( time * 100 + i* 50) * 0.5  * 0.2 + 0.6) ;
+		// starFieldMaterials[i].color.setHSV( 0, 0, 0.4 ) ;
+	
+	}
+	
+	renderer.clear();
+	renderer.render( scene, camera );
+
+}
+
+
+function unloadSceneIntro() {
+	
+	scene.remove(starFields);
+	document.removeEventListener( 'mousemove', onDocumentMouseMove, false );
+	document.removeEventListener( 'touchstart', onDocumentTouchStart, false );
+	document.removeEventListener( 'touchmove', onDocumentTouchMove, false );
+	
+}
+
+function setupSceneIntro() {
+	camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 120, 3000 );
+	camera.setLens(100);
+
+	scene = new THREE.Scene();
+	scene.fog = new THREE.FogExp2( 0x000000, 0.0007 );
+
+	var geometry = new THREE.Geometry();
+					
+	var halfStarFieldLength = starFieldLength / 2;
+
+	for ( i = 0; i < 30000; i ++ ) {
+
+		vector = new THREE.Vector3( Math.random() * starFieldLength - halfStarFieldLength, Math.random() * starFieldLength - halfStarFieldLength, Math.random() * starFieldLength - halfStarFieldLength );
+		
+		geometry.vertices.push( new THREE.Vertex( vector ) );
+
+	}
+
+	var starCanvas = generateStarCanvas();			
+	var starTexture = new THREE.Texture(starCanvas);
+	starTexture.needsUpdate = true;
+	
+	var z = -1000;
+
+	for ( i = 0; i < starFieldsCount; i ++ ) {
+
+		size = 8;
+
+
+		starFieldMaterials[i] = new THREE.ParticleBasicMaterial( { size: size, 
+			map: starTexture,
+			color: 0xffffff
+		} );
+		
+		var particles = new THREE.ParticleSystem( geometry, starFieldMaterials[i] );
+		particles.position.z = z;
+		z -= 500;
+		particles.rotation.x = Math.random() * 6;
+		particles.rotation.y = Math.random() * 6;
+		particles.rotation.z = Math.random() * 6;
+
+		starFields.add( particles );
+		
+
+	}
+	
+	renderCallback = renderIntro;
+	
+	scene.add(starFields);
+	
+	document.addEventListener( 'mousemove', onDocumentMouseMove, false );
+	document.addEventListener( 'touchstart', onDocumentTouchStart, false );
+	document.addEventListener( 'touchmove', onDocumentTouchMove, false );
+}
+
+function onDocumentMouseMove( event ) {
+
+	mouseX = event.clientX - windowHalfX;
+	mouseY = event.clientY - windowHalfY;
+
+}
+
+function onDocumentTouchStart( event ) {
+
+	if ( event.touches.length == 1 ) {
+
+		event.preventDefault();
+
+		mouseX = event.touches[ 0 ].pageX - windowHalfX;
+		mouseY = event.touches[ 0 ].pageY - windowHalfY;
+
+	}
+}
+
+function onDocumentTouchMove( event ) {
+
+	if ( event.touches.length == 1 ) {
+
+		event.preventDefault();
+
+		mouseX = event.touches[ 0 ].pageX - windowHalfX;
+		mouseY = event.touches[ 0 ].pageY - windowHalfY;
+
+	}
+
+}
+
+
 function generateStarCanvas() {
 
 	var canvas = document.createElement( 'canvas' );
@@ -37,7 +173,7 @@ function generateStarCanvas() {
 	// context.fillStyle = 'grey';
 
 	context.beginPath();
-	context.arc( 64, 64, 30, 0, Math.PI * 2, false) ;
+	context.arc( 64, 64, 24, 0, Math.PI * 2, false) ;
 	context.closePath();
 
 	//context.lineWidth = 0.5; //0.05
